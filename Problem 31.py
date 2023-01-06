6e07029091bb005f2df2c1d92c738b62e4a22fa0
Phil Hanna <ph1204@gmail.com>
2023-01-06 10:39:39 -0500

    Added comments and timing to problem 31

--- Problem 31.py
+++ Problem 31.py
@@ -7,14 +7,28 @@
 
 # Solution: Rather than using some generating function, this is a good target for dynamic programing.
 
+from time import time
+
+stime = time()
+
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-# Initialize our solution. This change vector is going to represent the number of ways to make the target value with only the coins allowed so far.
-change = [0 for x in range(0, 201)]
-change[0] = 1 #There's one way to make no money with no coins.
+# Initialize our solution.
+# This change vector is going to represent the number of ways
+# to make the target value with only the coins allowed so far.
 
-for coin in coins: #We'll start by building the answer to the question "How many ways can you make change with a penny", then adding in coins one by one.
+change = [0 for x in range(0, 201)]
+change[0] = 1  # There's one way to make no money with no coins.
+
+for coin in coins:
+    # We'll start by building the answer to the question
+    # "How many ways can you make change with a penny",
+    # then adding in coins one by one.
     for i in range(coin, 201):
-        change[i] = change[i-coin] + change[i] #The critical step.
+        change[i] = change[i - coin] + change[i]  # The critical step.
+
+etime = time()
+diff = 1000 * (etime - stime)
 
 print(f"There are {change[200]} ways to make change for 2 pounds with British coins.")
+print(f"Elapsed time = {diff} milliseconds")
